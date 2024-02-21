dab5468ed9e671328d6d8997300a0c7d0c97c791
gustav018 <gustavofleitas18@gmail.com>
2024-02-21 00:06:51 -0300

    Agregando archivos de la carpeta PrimerReact

---
+++ Primer React/script.js
@@ -0,0 +1,6 @@
+
+const App = () => {
+    return React.createElement("h1", {}, "Our First React page has rendered");
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
